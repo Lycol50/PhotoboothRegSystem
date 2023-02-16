7242c279b57f120203cf1193a097bee7adbe454b
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-02-16 11:57:23 +0800

    Add files via upload

--- action.php
+++ action.php
@@ -1,31 +1,271 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
+function onEmailSend(string $num)
+{
+    $font = "font-family: 'Bebas Neue', sans-serif;";
+    return '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml" lang="en">
+    <head><link rel="stylesheet" type="text/css" hs-webfonts="true" href="https://fonts.googleapis.com/css?family=Lato|Lato:i,b,bi">
+    <title>Thank You for Registering</title>
+    <meta property="og:title" content="Photobooth Registration">
+    
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <style type="text/css">
+
+    @import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+    
+    a{ 
+    text-decoration: underline;
+    color: inherit;
+    font-weight: bold;
+    color: #253342;
+    }
+    
+    h1 {
+        font-size: 65px;
+        ' .
+        $font .
+        '
+      }
+    
+    h2{
+    font-size: 28px;
+    font-weight: 900; 
+    }
+    
+    p {
+    font-weight: 100;
+    }
+    
+    td {
+    vertical-align: top;
+    }
+    
+    #email {
+    margin: auto;
+    width: 600px;
+    background-color: white;
+    }
+    
+    button{
+    font: inherit;
+    background-color: #FF7A59;
+    border: none;
+    padding: 10px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-weight: 900; 
+    color: white;
+    border-radius: 5px; 
+    box-shadow: 3px 3px #d94c53;
+    }
+    
+    .subtle-link {
+    font-size: 9px; 
+    text-transform:uppercase; 
+    letter-spacing: 1px;
+    color: #CBD6E2;
+    }
+    
+    </style>
+    
+    </head>
+    
+    <body bgcolor="#F5F8FA" style="width: 100%; margin: auto 0; padding:0; font-family:Lato, sans-serif; font-size:18px; color:#33475B; word-break:break-word">
+    
+    <! Banner --> 
+    <table role="presentation" width="100%">
+    <tr>
+    
+    <td bgcolor="#C41E3A" align="center" style="color: white;">
+    
+    <h1> Thank You! </h1>
+    
+    </td>
+    </table>
+    
+    
+    
+    
+    <! First Row --> 
+    
+    <table role="presentation" border="0" cellpadding="0" cellspacing="10px" style="padding: 30px 30px 30px 60px;">
+    <tr>
+    <td>
+    <h2> This is your serial number for Photobooth : ' .
+        $num .
+        ' </h2>
+    <p>
+    Please present this code to the booth to proceed with picture taking. To get the pictures, please approach to Registration booth to recieve pictures to your Informatics Email. 
+    </p>
+    </td> 
+    </tr>
+    </table>
+    <! Second Row --> 
+    
+    <table role="presentation" bgcolor="#EAF0F6" width="100%" >
+    <tr>
+    <td align="left" style="padding: 30px 30px;">
+    <p style="color:#99ACC2"> Made by Lyco Tatierra @ <a href="https://discord.gg/dw9V2gQytW" style="color:#99ACC2"> princepines </a></p>    
+    </td>
+    </tr>
+    </table> 
+    </div>
+    </body>
+    </html>';
+}
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require "PHPMailer/src/Exception.php";
+require "PHPMailer/src/PHPMailer.php";
+require "PHPMailer/src/SMTP.php";
+
+$mail = new PHPMailer(true);
+$name = $_POST["name"];
+$email = $_POST["email"];
+$optionPhoto = $_POST["option"];
 $serial = "Info-" . mt_rand(000001, 999999);
+$pass = ""; // password for smtp
 
 // Database connection
-$conn = new mysqli('localhost','root','','photobooth');
-if($conn->connect_error){
-    die('Connection Failed : '.$conn->connect_error);
-}else{
-    $stmt = $conn->prepare("insert into photobooth_data (serial, name, email) values (?, ?, ?)");
-    $stmt->bind_param("sss", $serial, $name, $email);
+$conn = new mysqli("address", "username", "password", "database"); // change to your database
+if ($conn->connect_error) {
+    die("Connection Failed : " . $conn->connect_error);
+} else {
+    $stmt = $conn->prepare(
+        "insert into photobooth_data (serial, name, email, optionPhoto) values (?, ?, ?, ?)"
+    );
+    $stmt->bind_param("ssss", $serial, $name, $email, $optionPhoto);
     $stmt->execute();
     mkdir("pictures/$serial", 0777, true);
+    try {
+        // Server setting for office365
+        $mail->SMTPDebug = 0; // Enable verbose debug output
+        $mail->isSMTP(); // Send using SMTP
+        $mail->Host = ""; // Set the SMTP server to send through
+        $mail->SMTPAuth = true; // Enable SMTP authentication
+        $mail->Username = ""; // username/email for smtp
+        $mail->Password = $pass;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port = 587; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $mail->isHTML(true); // Set email format to HTML
+        // Recipients
+        $mail->setFrom("email", "Photobooth"); // email here and set name as
+        $mail->addAddress($email); // Add a recipient
+        $mail->Subject = "Thank You for Registering!";
+        $mail->Body = onEmailSend($serial);
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
     $stmt->close();
     $conn->close();
 }
 ?>
 <html>
 <head>
-    <title></title>
+    <link rel="icon" type="image/x-icon" href="icon.ico">
+    <title>Thank You! - Photobooth</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="styles.css" rel="stylesheet">
+    <style type="text/css">
+
+      @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+   
+      a{ 
+        text-decoration: underline;
+        color: inherit;
+        font-weight: bold;
+        color: #253342;
+      }
+      
+      h1 {
+        font-size: 65px;
+        font-family: 'Bebas Neue', cursive;
+      }
+      
+        h2{
+        font-size: 28px;
+        font-weight: 900; 
+      }
+      
+      p {
+        font-weight: 100;
+      }
+      
+      td {
+        vertical-align: top;
+        padding: 20px;
+      }
+      
+      #email {
+        margin: auto;
+        width: 600px;
+        background-color: white;
+      }
+      
+      button{
+        font: inherit;
+        background-color: #FF7A59;
+        border: none;
+        padding: 10px;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        font-weight: 900; 
+        color: white;
+        border-radius: 5px; 
+        box-shadow: 3px 3px #d94c53;
+      }
+      
+      .subtle-link {
+        font-size: 9px; 
+        text-transform:uppercase; 
+        letter-spacing: 1px;
+        color: #CBD6E2;
+      }
+      
+    </style>
 </head>
-<body class="padding">
-    <h1 class="fw-bold">Thank you!</h1>
-    <h2 class="display-5">Register Code: <?php echo $serial; ?></h2>
-    <h3 class="">Please present this page to the registration area on photobooth</h3>
-</body>
+<body bgcolor="#F5F8FA" style="width: 100%; margin: auto 0; padding:0; font-family:Lato, sans-serif; font-size:18px; color:#33475B; word-break:break-word">
+  
+  <! Banner --> 
+         <table role="presentation" width="100%">
+            <tr>
+         
+              <td bgcolor="#C41E3A" align="center" style="color: white;">
+                
+                <h1> Thank You! </h1>
+                
+              </td>
+        </table>
+
+    <! First Row --> 
+  <table role="presentation" border="0" cellpadding="0" cellspacing="10px" style="padding: 30px 30px 30px 60px;">
+     <tr>
+       <td>
+        <h2> This is your serial number for Photobooth : <?php echo $serial; ?></h2>
+            <p>
+              Please present this code to the booth to proceed with picture taking. To get the pictures, please approach to Registration booth to recieve pictures to your Informatics Email. (This confirmation is sent to your email.)
+            </p>
+          </td> 
+          </tr>
+                 </table>
+
+        <! Footer --> 
+  <table role="presentation" bgcolor="#EAF0F6" width="100%" >
+      <tr>
+          <td align="left" style="padding: 30px 30px;">
+          <p style="color:#99ACC2"><img src="pines.png" width="25" height="25"> Made by Lyco Tatierra @ <a href="https://discord.gg/dw9V2gQytW" style="color:#99ACC2"> princepines </a></p>    
+          </td>
+          </tr>
+      </table> 
+      </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,117 @@
-<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" type="image/x-icon" href="icon.ico">
+    <title>Photobooth Registration</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="styles.css" rel="stylesheet">
+    <style type="text/css">
+
+      @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+   
+      a{ 
+        text-decoration: underline;
+        color: inherit;
+        font-weight: bold;
+        color: #253342;
+      }
+      
+      h1 {
+        font-size: 65px;
+        font-family: 'Bebas Neue', cursive;
+      }
+      
+        h2{
+        font-size: 28px;
+        font-weight: 900; 
+      }
+      
+      p {
+        font-weight: 100;
+      }
+      
+      td {
+        vertical-align: top;
+        padding: 20px;
+      }
+      
+      #email {
+        margin: auto;
+        width: 600px;
+        background-color: white;
+      }
+      
+      .subtle-link {
+        font-size: 9px; 
+        text-transform:uppercase; 
+        letter-spacing: 1px;
+        color: #CBD6E2;
+      }
+      
+    </style>
+</head>
+<body bgcolor="#F5F8FA" style="width: 100%; margin: auto 0; padding:0; font-family:Lato, sans-serif; font-size:18px; color:#33475B; word-break:break-word">
+  
+  <! Banner --> 
+         <table role="presentation" width="100%">
+            <tr>
+         
+              <td bgcolor="#C41E3A" align="center" style="color: white;">
+                
+                <h1> Valentines Photobooth </h1>
+                
+              </td>
+        </table>
+  
+  
+  
+  
+    <! First Row --> 
+  
+  <table role="presentation" border="0" cellpadding="0" cellspacing="10px" style="padding: 30px 30px 30px 60px;">
+     <tr>
+       <td>
+        <h2>Please fill up this form</h2>
+            <form action="action.php" method="post">
+                <div class="mb-3">    
+                    <label for="name" class="form-label">Name:</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Enter your name" style="width: 100%" required>
+                </div>
+                <div class="mb-3">  
+                    <label for="name" class="form-label">Active Email:</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Enter your email" style="width: 100%" required>
+                </div>
+                <div class="mb-3">  
+                  <p class="form-label">Choose your option:</p>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" id="1shot" name="option" value="1shot">
+                    <label class="form-check-label for="option" class="form-label">1 Shot : 10 Pesos (with frame and props)</label></div>
+                  <div class="form-check">  
+                    <input class="form-check-input" type="radio" id="2shot" name="option" value="2shot">
+                    <label class="form-check-label for="option" class="form-label">2 Shot : 20 Pesos (with frame and props)</label></div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" id="4shot" name="option" value="4shot">
+                    <label class="form-check-label for="option" class="form-label">4 Shot : 50 Pesos (with frame and props)</label></div>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+          </td> 
+          </tr>
+                 </table>
+
+        <! Footer --> 
+  <table role="presentation" bgcolor="#EAF0F6" width="100%" >
+      <tr>
+          <td align="left" style="padding: 30px 30px;">
+            <p style="color:#99ACC2"><img src="pines.png" width="25" height="25"> Made by Lyco Tatierra @ <a href="https://discord.gg/dw9V2gQytW" style="color:#99ACC2"> princepines </a></p>    
+          </td>
+          </tr>
+      </table> 
+      </div>
+    </body>
+</html>
+
+<!-- <!DOCTYPE html>
 <html>
 <head>
     <title></title>
@@ -21,4 +134,4 @@
     </div>
         <button type="submit" class="btn btn-primary">Submit</button>
 </body>
-</html>
+</html> -->
